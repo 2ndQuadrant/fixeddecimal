6c980c9910bb9b44c69da99286f532a1532e859e
David Rowley <dgrowley@gmail.com>
2015-08-31 17:57:30 +1200

    Add missing overflow.sql

---
+++ test/sql/overflow.sql
@@ -0,0 +1,34 @@
+-- Ensure the expected extreme values can be represented
+SELECT '-92233720368547758.08'::fixeddecimal as minvalue,'92233720368547758.07'::fixeddecimal as maxvalue;
+
+SELECT '-92233720368547758.09'::fixeddecimal;
+
+SELECT '92233720368547758.08'::fixeddecimal;
+
+SELECT '-92233720368547758.08'::fixeddecimal - '0.01'::fixeddecimal;
+
+SELECT '92233720368547758.07'::fixeddecimal + '0.01'::fixeddecimal;
+
+-- Ensure limits of int2 can be represented
+SELECT '32767'::fixeddecimal::int2,'-32768'::fixeddecimal::int2;
+
+-- Ensure overflow of int2 is detected
+SELECT '32768'::fixeddecimal::int2;
+
+-- Ensure underflow of int2 is detected
+SELECT '-32769'::fixeddecimal::int2;
+
+-- Ensure limits of int4 can be represented
+SELECT '2147483647'::fixeddecimal::int4,'-2147483648'::fixeddecimal::int4;
+
+-- Ensure overflow of int4 is detected
+SELECT '2147483648'::fixeddecimal::int4;
+
+-- Ensure underflow of int4 is detected
+SELECT '-2147483649'::fixeddecimal::int4;
+
+-- Ensure overflow is detected
+SELECT SUM(a) FROM (VALUES('92233720368547758.07'::fixeddecimal),('0.01'::fixeddecimal)) a(a);
+
+-- Ensure underflow is detected
+SELECT SUM(a) FROM (VALUES('-92233720368547758.08'::fixeddecimal),('-0.01'::fixeddecimal)) a(a);
